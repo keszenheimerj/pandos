f50869da3734fe421a0d4b1ec970cbd3689bc4bb
keszenheimerj <keszenheimerj@xavier.edu>
2022-08-31 18:50:19 -0400

    added insertProcQ and removeProcQ to the pcbQueues file. Also added pcb_t structure to types.h

--- h/types.h
+++ h/types.h
@@ -67,6 +67,27 @@ typedef struct state_t {
 
 } state_t, *state_PTR;
 
+/* process control block type */
+typedef struct pcb_t {
+	/* process queue fields */
+	struct pcb_t 	*p_next, 	/* pointer to next entry */
+			*p_prev, 	/* pointer to prev entry */
+			
+	/* process tree fields */
+			*p_prnt, 	/* pointer to parent */
+			*p_child, 	/* pointer to 1st child */
+			*p_sib; 	/* pointer to sibling */
+			
+	/* process status information */
+	state_t 	p_s; 		/* processor state */
+	cpu_t 		p_time; 	/* cpu time used by proc */
+	int 		*p_semAdd; 	/* pointer to sema4 on */
+					/* which process blocked */
+	/* support layer information */
+	support_t *p supportStruct;
+					/* ptr to support struct */
+} pcb_t, *pcb_PTR;
+
 #define	s_at	s_reg[0]
 #define	s_v0	s_reg[1]
 #define s_v1	s_reg[2]
