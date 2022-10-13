6b57a33936223608f651eff16250986a57ea722c
Evan Hanson <sakhevan@gmail.com>
2022-10-13 11:52:39 -0400

    psudo code for sys 6

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -33,6 +33,18 @@ HIDDEN void VERHOGEN((sem_PTR sema4){
 	LDST(sema4);/*return to current proccess*/
 }
 
+/*sys6*/
+HIDDEN void GETCPUTIME(){
+	/*Bookeeping and management
+		look for a call called "CPU Time" maybe...
+		Write down time of day clock
+		Do subtraction to figure out how much time was used
+		Return time
+		How much is there plus how much current time slice.
+		use register s_v0
+		*/
+}
+
 /*sys8*/
 HIDDEN void WAITFORIODEVICE(special sema4){
 	/*find which device
